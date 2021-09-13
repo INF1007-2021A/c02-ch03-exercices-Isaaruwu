696fb1c25f8397dad8c8288e51c40c1095d8fb7e
Isaaruwu <ismailaarabb@gmail.com>
2021-09-13 17:24:46 -0400

    Update exercice.py
    Exercice 2 et 3 complété

--- exercice.py
+++ exercice.py
@@ -6,16 +6,25 @@ def dissipated_power(voltage, resistance):
 	power_diss = voltage**2 / resistance
 	return power_diss
 
+
 def orthogonal(v1, v2):
 	# TODO: Retourner vrai si les vecteurs sont orthogonaux, faux sinon.
-	v1[0] # Pour accéder au X
-	v1[1] # Pour accéder au Y
-	pass
+	# v1[0] # Pour accéder au X
+	# v1[1] # Pour accéder au Y
+
+	produit_scalaire = v1[0] * v2[0] + v1[1] * v2[1]
+	return produit_scalaire == 0
+
 
 def average(values):
 	# TODO: Calculer la moyenne des valeurs positives (on ignore les valeurs strictement négatives).
+	sum = nbr_element = 0
 	for v in values:
-		pass # La variable v contient une valeur de la liste.
+		if v > 0:
+			nbr_element += 1
+			sum += v
+	average_sum = sum / nbr_element
+	return average_sum
 
 def bills(value):
 	# TODO: Calculez le nombre de billets de 20$, 10$ et 5$ et pièces de 1$ à remettre pour représenter la valeur.
